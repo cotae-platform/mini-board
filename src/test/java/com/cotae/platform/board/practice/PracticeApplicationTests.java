318d1b025e256fdace059c64933570bc8432b1ce
TrulyNotMalware <notleebutyee@gmail.com>
2022-12-20 20:21:16 +0900

    Init : Source initialize
    - Source initialized.
    - Spring boot 2.7.6
    no related issue

---
+++ src/test/java/com/cotae/platform/board/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotae.platform.board.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
